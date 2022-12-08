09eef03d59490200ffbfe2a3705744e1ee2318a5
venkatama <manikanta.guntur@outlook.com>
2022-12-08 20:15:32 +0530

    updated the list

--- Telnet Scripts/telnet_configure_multiple_devices.py
+++ Telnet Scripts/telnet_configure_multiple_devices.py
@@ -1,9 +1,9 @@
 import telnetlib
 import time
 
-router1 = {'host': '192.168.122.10', 'user': 'u1', 'password': 'cisco'}
-router2 = {'host': '192.168.122.20', 'user': 'u1', 'password': 'cisco'}
-router3 = {'host': '192.168.122.30', 'user': 'u1', 'password': 'cisco'}
+router1 = {'host': '10.1.1.20', 'user': 'u1', 'password': 'cisco'}
+router2 = {'host': '10.1.1.30', 'user': 'u1', 'password': 'cisco'}
+router3 = {'host': '10.1.1.40', 'user': 'u1', 'password': 'cisco'}
 
 routers = [router1, router2, router3]
 
@@ -19,11 +19,9 @@ for router in routers:
     tn.write(router['password'].encode() + b'\n')
 
     tn.write(b'terminal length 0\n')
-    tn.write(b'enable\n')
-    tn.write(b'cisco\n')  # this is the enable password
 
     tn.write(b'conf t\n')
-    tn.write(b'ip route 0.0.0.0 0.0.0.0 e0/0 10.1.1.2\n')
+    tn.write(b'ip route 0.0.0.0 0.0.0.0 FastEthernet0/0 10.1.1.2\n')
     tn.write(b'end\n')
     tn.write(b'show ip route\n')
     tn.write(b'exit\n')
